c6126af35f932d5ed20c6b4310c75208be9a124f
ASTREE\geoffreyR <geoffrey.robert@astree-software.fr>
2018-12-07 11:51:04 +0100

    Fix l'heritage de DefaultHyperSolver
    Extension de DefaultSolver par soucis de concision. Le field scope
    herite de type SolverScope est caste a chaque appel a ses
    fonctionnalites specifiques.

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/hypersolver/scope/HyperSolverScope.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/hypersolver/scope/HyperSolverScope.java
@@ -1,4 +1,37 @@
 package org.optaplanner.core.impl.hypersolver.scope;
 
-public class HyperSolverScope<Solution> {
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.impl.solver.scope.DefaultSolverScope;
+
+public class HyperSolverScope<Solution_> extends DefaultSolverScope {
+
+    protected volatile Solution_ restartSolution;
+
+    // ************************************************************************
+    // Constructors and simple getters/setters
+    // ************************************************************************
+
+    public Solution_ getRestartSolution() {
+        return restartSolution;
+    }
+
+    /**
+     * The {@link PlanningSolution restart solution} must never be the same instance
+     * as the {@link PlanningSolution working solution} or {@link PlanningSolution best
+     * solution} it should be a (un)changed clone.
+     * @param bestSolution never null
+     */
+    public void setRestartSolution(Solution_ bestSolution) {
+        this.bestSolution = bestSolution;
+    }
+
+    // ************************************************************************
+    // Calculated methods
+    // ************************************************************************
+
+    public void setRestartSolutionFromBestSolution() {
+        // The workingSolution must never be the same instance as the bestSolution.
+        scoreDirector.setWorkingSolution(scoreDirector.cloneSolution(restartSolution));
+    }
+
 }
