4bd551e01d475acbc65448ee7fcb52092b7415d5
ASTREE\geoffreyR <geoffrey.robert@astree-software.fr>
2018-12-07 11:51:04 +0100

    Fix l'heritage de DefaultHyperSolver
    Extension de DefaultSolver par soucis de concision. Le field scope
    herite de type SolverScope est caste a chaque appel a ses
    fonctionnalites specifiques.

--- optaplanner-core/src/main/java/org/optaplanner/core/impl/hypersolver/DefaultHyperSolver.java
+++ optaplanner-core/src/main/java/org/optaplanner/core/impl/hypersolver/DefaultHyperSolver.java
@@ -6,16 +6,15 @@ import org.optaplanner.core.config.solver.EnvironmentMode;
 import org.optaplanner.core.impl.hypersolver.scope.HyperSolverScope;
 import org.optaplanner.core.impl.hypersolver.switcher.HyperSolverSwitcher;
 import org.optaplanner.core.impl.phase.Phase;
-import org.optaplanner.core.impl.solver.AbstractSolver;
+import org.optaplanner.core.impl.solver.DefaultSolver;
 import org.optaplanner.core.impl.solver.random.RandomFactory;
 import org.optaplanner.core.impl.solver.recaller.BestSolutionRecaller;
 import org.optaplanner.core.impl.solver.scope.DefaultSolverScope;
 import org.optaplanner.core.impl.solver.termination.BasicPlumbingTermination;
 import org.optaplanner.core.impl.solver.termination.Termination;
 
-public class DefaultHyperSolver<Solution_> extends AbstractSolver<Solution_> {
+public class DefaultHyperSolver<Solution_> extends DefaultSolver<Solution_> {
 
-    protected final HyperSolverScope<Solution_> solverScope;
     protected final HyperSolverSwitcher<Solution_> switcher;
 
     // ************************************************************************
@@ -24,13 +23,10 @@ public class DefaultHyperSolver<Solution_> extends AbstractSolver<Solution_> {
 
     public DefaultHyperSolver(EnvironmentMode environmentMode, RandomFactory randomFactory,
                          BestSolutionRecaller<Solution_> bestSolutionRecaller, BasicPlumbingTermination basicPlumbingTermination, Termination termination,
-                         List<Phase<Solution_>> phaseList, DefaultSolverScope<Solution_> solverScope,
+                         List<Phase<Solution_>> phaseList, HyperSolverScope<Solution_> solverScope,
                          HyperSolverSwitcher<Solution_> switcher) {
-        super(bestSolutionRecaller, termination, bestSolutionRecaller, basicPlumbingTermination, termination);
-        this.environmentMode = environmentMode;
-        this.randomFactory = randomFactory;
-        this.basicPlumbingTermination = basicPlumbingTermination;
-        this.solverScope = solverScope;
+        super(environmentMode, randomFactory, bestSolutionRecaller, basicPlumbingTermination, termination,
+                null, solverScope);
         this.switcher = switcher;
     }
 
@@ -38,8 +34,12 @@ public class DefaultHyperSolver<Solution_> extends AbstractSolver<Solution_> {
         return switcher;
     }
 
-    public void setDecider(HyperSolverSwitcher<Solution_> switcher) {
-        this.switcher = switcher;
+    // ************************************************************************
+    // Complex getters
+    // ************************************************************************
+
+    public Solution_ getRestartSolution() {
+        return ((HyperSolverScope<Solution_>) solverScope).getRestartSolution();
     }
 
     // ************************************************************************
@@ -55,17 +55,17 @@ public class DefaultHyperSolver<Solution_> extends AbstractSolver<Solution_> {
     @Override
     public void runPhases(DefaultSolverScope<Solution_> solverScope) {
         while (!termination.isSolverTerminated(solverScope)) {
-            switcher.processNewPhase(solverScope);
             Phase<Solution_> phase = switcher.switchToNextPhase(solverScope);
             // TODO gérer la récupération de data pour le learner
             phase.solve(solverScope);
+            switcher.processNewPhase(solverScope);
         }
     }
 
     @Override
     public void solvingEnded(DefaultSolverScope<Solution_> solverScope) {
-        super.solvingEnded(solverScope);
         switcher.solvingEnded(solverScope);
+        super.solvingEnded(solverScope);
     }
 
 }
